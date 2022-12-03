f568adb104d8ef404966931dc03e9e799c73de9c
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-12-03 14:19:17 +0100

    solution: optimize day 03
    * remove dep on itertools

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,37 +1,39 @@
-use itertools::Itertools;
+type Input<'a> = Vec<&'a str>;
+
+fn parse(input: &str) -> Input {
+    input.lines().collect()
+}
 
 fn score_char(code: u8) -> u8 {
     code % 32 + (26 * (code <= 90) as u8)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &Input) -> Option<u32> {
     let prios = input
-        .lines()
+        .iter()
         .filter_map(|l| {
             let parts = l.split_at(l.len() / 2);
             let a = parts.0.as_bytes();
             let b = parts.1.as_bytes();
             a.iter()
-                .find(|x| b.contains(x))
-                .map(|x| score_char(*x) as u32)
+                .find(|byte| b.contains(byte))
+                .map(|&byte| score_char(byte) as u32)
         })
         .sum();
-
     Some(prios)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &Input) -> Option<u32> {
     let prios = input
-        .lines()
         .chunks(3)
-        .into_iter()
-        .filter_map(|mut chunks| {
+        .filter_map(|chunks| {
+            let mut chunks = chunks.iter();
             let a = chunks.next()?.as_bytes();
             let b = chunks.next()?.as_bytes();
             let c = chunks.next()?.as_bytes();
             a.iter()
-                .find(|x| b.contains(x) && c.contains(x))
-                .map(|x| score_char(*x) as u32)
+                .find(|byte| b.contains(byte) && c.contains(byte))
+                .map(|&byte| score_char(byte) as u32)
         })
         .sum();
     Some(prios)
@@ -39,8 +41,9 @@ pub fn part_two(input: &str) -> Option<u32> {
 
 fn main() {
     let input = &advent_of_code::read_file("inputs", 3);
-    advent_of_code::solve!(1, part_one, input);
-    advent_of_code::solve!(2, part_two, input);
+    let parsed = advent_of_code::parse!(parse, input);
+    advent_of_code::solve!(1, part_one, &parsed);
+    advent_of_code::solve!(2, part_two, &parsed);
 }
 
 #[cfg(test)]
@@ -50,12 +53,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_one(&input), Some(157));
+        assert_eq!(part_one(&parse(&input)), Some(157));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 3);
-        assert_eq!(part_two(&input), Some(70));
+        assert_eq!(part_two(&parse(&input)), Some(70));
     }
 }
