69401dd9ba4beb5a9a06d5598051d2f05abd20a8
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-12-03 12:44:00 +0100

    solution: optimize day 03
    * replace HashSet impl. with faster implementation

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,9 +1,4 @@
 use itertools::Itertools;
-use std::collections::HashSet;
-
-fn to_byte_set(s: &str) -> HashSet<&u8> {
-    HashSet::from_iter(s.as_bytes())
-}
 
 fn score_char(code: u8) -> u8 {
     code % 32 + (26 * (code <= 90) as u8)
@@ -14,11 +9,14 @@ pub fn part_one(input: &str) -> Option<u32> {
         .lines()
         .filter_map(|l| {
             let parts = l.split_at(l.len() / 2);
-            let a = to_byte_set(parts.0);
-            let b = to_byte_set(parts.1);
-            Some(score_char(**a.intersection(&b).next()?) as u32)
+            let a = parts.0.as_bytes();
+            let b = parts.1.as_bytes();
+            a.iter()
+                .find(|x| b.contains(x))
+                .map(|x| score_char(*x) as u32)
         })
         .sum();
+
     Some(prios)
 }
 
@@ -27,11 +25,13 @@ pub fn part_two(input: &str) -> Option<u32> {
         .lines()
         .chunks(3)
         .into_iter()
-        .filter_map(|chunks| {
-            let matches = chunks
-                .map(to_byte_set)
-                .reduce(|acc, chunk| acc.intersection(&chunk).copied().collect())?;
-            Some(score_char(**matches.iter().next()?) as u32)
+        .filter_map(|mut chunks| {
+            let a = chunks.next()?.as_bytes();
+            let b = chunks.next()?.as_bytes();
+            let c = chunks.next()?.as_bytes();
+            a.iter()
+                .find(|x| b.contains(x) && c.contains(x))
+                .map(|x| score_char(*x) as u32)
         })
         .sum();
     Some(prios)
